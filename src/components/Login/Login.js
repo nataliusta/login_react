cc00ae6e63575a476a8730930cd92e6f6e1a9527
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-14 18:25:30 +0300

    Add useContext to Login.js and Home.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -24,7 +25,8 @@ const passwordReducer = (state, action) => {
   return {value: '', isValid: false};
 };
 
-const Login = (props) => {
+const Login = () => {
+  
   // const [enteredEmail, setEnteredEmail] = useState('');
  // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -40,6 +42,8 @@ const Login = (props) => {
     value: '', 
     isValid: null,
  });
+
+ const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -92,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
